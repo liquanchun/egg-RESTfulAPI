ad52f5c30c0d6851df7cd14365924ce80edcee90
APAC\listev <350966707@qq.com>
2018-12-06 09:44:01 +0800

    update

--- config/config.default.js
+++ config/config.default.js
@@ -32,13 +32,13 @@ module.exports = appInfo => {
     // 单数据库信息配置
     client: {
       // host
-      host: '193.112.155.251',
+      host: '1111111',
       // 端口号
       port: '3306',
       // 用户名
       user: 'root',
       // 密码
-      password: 'KGFKO8dtZt',
+      password: '*******',
       // 数据库名
       database: 'car_app',
     },
